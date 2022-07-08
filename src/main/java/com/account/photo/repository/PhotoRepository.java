052ec5eef79727d39cba723b0e208ccbeb0d926c
jEUGENEdev <89359545+jEUGENEdev@users.noreply.github.com>
2022-07-08 04:48:14 +0300

    Add files via upload

--- src/main/java/com/account/photo/repository/PhotoRepository.java
+++ src/main/java/com/account/photo/repository/PhotoRepository.java
@@ -2,8 +2,13 @@ package com.account.photo.repository;
 
 import com.account.photo.entity.Photo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PhotoRepository extends JpaRepository<Photo, Long> {
+    @Query("FROM Photo WHERE id >= :start AND id <= :stop")
+    List<Photo> findByIdRange(long start, long stop);
 }
