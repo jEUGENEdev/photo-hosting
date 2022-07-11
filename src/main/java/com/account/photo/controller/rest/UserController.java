44cf8e38dbd0c8e0bf775235da20e398d065cda2
jEUGENEdev <89359545+jEUGENEdev@users.noreply.github.com>
2022-07-11 06:11:37 +0300

    Add files via upload

---
+++ src/main/java/com/account/photo/controller/rest/UserController.java
@@ -0,0 +1,22 @@
+package com.account.photo.controller.rest;
+
+import com.account.photo.model.UserModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserModel userModel;
+
+    @PostMapping("create")
+    public String create(@RequestParam(required = false, name = "vk_id") String vkId,
+                         @RequestParam(required = false) String username,
+                         @RequestParam(required = false) String role) {
+        return userModel.create(vkId, username, role);
+    }
+}
