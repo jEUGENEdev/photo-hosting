9d0da9cff6af5b7bceb8cf9f38292f5b90adce54
jEUGENEdev <89359545+jEUGENEdev@users.noreply.github.com>
2022-07-11 06:08:15 +0300

    Add files via upload

--- src/main/resources/db/migration/V1__Init.sql
+++ src/main/resources/db/migration/V1__Init.sql
@@ -3,19 +3,20 @@ insert into hibernate_sequence values ( 1 );
 
 
 create table photo (
-    id bigint primary key not null,
+    id bigint primary key not null auto_increment,
     full_name text,
     name text
 );
 
 create table user (
-    id bigint primary key not null,
+    id bigint primary key not null auto_increment,
     password varchar(255),
     role varchar(255),
-    username varchar(255)
+    username varchar(255),
+    vk_id bigint unique not null
 );
 
-insert into user (id, password, role, username) values (1, 'qNfjs493jfNru*hf', 'ROLE_CREATOR', 'creator');
+insert into user (id, password, role, username, vk_id) values (1, 'qNfjs493jfNru*hf', 'ROLE_CREATOR', 'creator', 145441435);
 
 create table photo_users (
     photo_id bigint not null,
