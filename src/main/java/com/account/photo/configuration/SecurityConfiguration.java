589ee997708e1e76e2091cc916cfea2fad3f8db2
jEUGENEdev <89359545+jEUGENEdev@users.noreply.github.com>
2022-07-16 02:13:21 +0300

    Add files via upload

--- src/main/java/com/account/photo/configuration/SecurityConfiguration.java
+++ src/main/java/com/account/photo/configuration/SecurityConfiguration.java
@@ -1,31 +1,31 @@
 package com.account.photo.configuration;
 
-import com.account.photo.security.UserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-    @Autowired
-    private UserDetailsService userDetailsService;
     @Value("${upload.photo.url}")
     private String uploadPhotoUrl;
+    @Autowired
+    private AuthenticationProvider authenticationProvider;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
-                .userDetailsService(userDetailsService)
+                .authenticationProvider(authenticationProvider)
                 .authorizeRequests()
                 .antMatchers("/", "/static/**", String.format("%s/**", uploadPhotoUrl), "/login", "/auth",
                         "/archive", "/photos/*", "/users/**").permitAll()
+                .antMatchers("/auth/vk").hasIpAddress("87.240.137.158")
                 .anyRequest().hasAnyRole("USER", "CREATOR", "ADMIN", "MODER")
                 .and()
                 .formLogin().loginPage("/login").loginProcessingUrl("/auth")
