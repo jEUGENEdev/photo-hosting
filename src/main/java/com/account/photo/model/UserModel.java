a10a97036676b9186edebb67a2012a07301f1642
jEUGENEdev <89359545+jEUGENEdev@users.noreply.github.com>
2022-07-15 22:50:40 +0300

    Add files via upload

--- src/main/java/com/account/photo/model/UserModel.java
+++ src/main/java/com/account/photo/model/UserModel.java
@@ -2,11 +2,10 @@ package com.account.photo.model;
 
 import com.account.photo.entity.User;
 import com.account.photo.repository.UserRepository;
+import com.account.photo.utils.Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
-
-import java.util.Random;
 
 @Component
 public class UserModel {
@@ -29,20 +28,12 @@ public class UserModel {
                     case 2 -> role = "ROLE_ADMIN";
                     default -> throw new RuntimeException();
                 }
-                String password = passwordGenerator();
+                String password = Utils.passwordGenerator();
                 userRepository.save(new User(vkID, username, passwordEncoder.encode(password), role, true));
                 return String.format("{\"status\": \"ok\", \"username\": \"%s\", \"password\": \"%s\"}", username, password);
             }
         } catch (RuntimeException e) {
             return "{\"status\": \"err\"}";
         }
-    }
-
-    private String passwordGenerator() {
-        Random random = new Random();
-        StringBuilder pass = new StringBuilder();
-        for(int i=0; i<10; i++)
-            pass.append((char) (random.nextInt(43) + 48));
-        return pass.toString();
     }
 }
