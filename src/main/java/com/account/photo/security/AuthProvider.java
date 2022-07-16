3aa6adfdc7976205abf1b815a8f59df32e5e2fca
jEUGENEdev <89359545+jEUGENEdev@users.noreply.github.com>
2022-07-16 03:46:47 +0300

    Add files via upload

--- src/main/java/com/account/photo/security/AuthProvider.java
+++ src/main/java/com/account/photo/security/AuthProvider.java
@@ -6,17 +6,20 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthProvider implements AuthenticationProvider {
     @Autowired
     private UserDetailsService userDetailsService;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         UserDetails user = (UserDetails) userDetailsService.loadUserByUsername(authentication.getName());
-        if(!user.getPassword().equals(authentication.getCredentials()))
+        if(!user.getPassword().equals(passwordEncoder.encode(authentication.getCredentials().toString())))
             throw new BadCredentialsException("Error password!");
         return new UsernamePasswordAuthenticationToken(user, user.getPassword(), user.getAuthorities());
     }
