0935422a04ca11841bffc703d8c963b6ab52c4d4
jEUGENEdev <89359545+jEUGENEdev@users.noreply.github.com>
2022-07-17 02:06:05 +0300

    Add files via upload

--- src/main/java/com/account/photo/utils/Utils.java
+++ src/main/java/com/account/photo/utils/Utils.java
@@ -1,5 +1,9 @@
 package com.account.photo.utils;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.HexFormat;
 import java.util.Random;
 
 public class Utils {
@@ -9,5 +13,12 @@ public class Utils {
         for(int i=0; i<10; i++)
             pass.append((char) (random.nextInt(43) + 48));
         return pass.toString();
+    }
+
+    public static boolean checkVkHash(long userId, long appId, String secretKey, String hash) throws NoSuchAlgorithmException {
+        MessageDigest md5 = MessageDigest.getInstance("MD5");
+        byte[] genHash = md5.digest(String.join("", appId+"", userId+"", secretKey).getBytes(StandardCharsets.UTF_8));
+        String resHash = HexFormat.of().formatHex(genHash);
+        return resHash.equals(hash);
     }
 }
