06d64d32c71f82e145b6346f87a561626b476854
jEUGENEdev <89359545+jEUGENEdev@users.noreply.github.com>
2022-07-16 03:47:16 +0300

    Add files via upload

--- src/main/java/com/account/photo/configuration/SecurityConfiguration.java
+++ src/main/java/com/account/photo/configuration/SecurityConfiguration.java
@@ -2,14 +2,10 @@ package com.account.photo.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -32,10 +28,5 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .formLogin().loginPage("/login").loginProcessingUrl("/auth")
                 .and()
                 .logout().logoutUrl("/logout");
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
