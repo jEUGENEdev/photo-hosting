7b7d6cfb7f2b8bca2ba429dc875540fcc9accf78
jEUGENEdev <89359545+jEUGENEdev@users.noreply.github.com>
2022-07-17 02:04:33 +0300

    Add files via upload

--- src/main/java/com/account/photo/model/AuthVkModel.java
+++ src/main/java/com/account/photo/model/AuthVkModel.java
@@ -3,12 +3,10 @@ package com.account.photo.model;
 import com.account.photo.entity.User;
 import com.account.photo.repository.UserRepository;
 import com.account.photo.security.UserDetails;
-import com.account.photo.utils.Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
@@ -18,15 +16,13 @@ public class AuthVkModel {
     @Autowired
     private UserRepository userRepository;
     @Autowired
-    private PasswordEncoder passwordEncoder;
-    @Autowired
     private AuthenticationProvider authenticationProvider;
 
     public void auth(long uid, String firstName) {
         Optional<User> user = userRepository.findByVkId(uid);
         User newUser;
         if(user.isEmpty()) {
-            newUser = new User(uid, firstName + (userRepository.count() + 1), passwordEncoder.encode(Utils.passwordGenerator()), "ROLE_USER", true);
+            newUser = new User(uid, firstName + (userRepository.count() + 1), null, "ROLE_USER", true);
             userRepository.save(newUser);
         }
         else
