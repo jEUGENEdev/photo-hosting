d5d5987ff3d5d45136717cb88ea08c1c6dff729d
jEUGENEdev <89359545+jEUGENEdev@users.noreply.github.com>
2022-07-16 03:24:48 +0300

    Add files via upload

--- src/main/java/com/account/photo/controller/FileUpdateController.java
+++ src/main/java/com/account/photo/controller/FileUpdateController.java
@@ -1,39 +1,22 @@
 package com.account.photo.controller;
 
-import com.account.photo.entity.Photo;
-import com.account.photo.repository.PhotoRepository;
+import com.account.photo.model.FileUpdateModel;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.UUID;
 
 @RequestMapping("/upload")
 @Controller
 public class FileUpdateController {
-    @Value("${upload.photo}")
-    private String uploadPhoto;
-    @Value("${upload.photo.url}")
-    private String uploadPhotoUrl;
     @Autowired
-    private PhotoRepository photoRepository;
+    private FileUpdateModel fileUpdateModel;
 
     @PostMapping("/n/photo")
     public String photo(MultipartFile file) throws IOException {
-        String normalName = file.getOriginalFilename();
-        String name = URLEncoder.encode(UUID.randomUUID() + "_" + normalName, StandardCharsets.UTF_8)
-                .replaceAll("[ -]+", "_");
-        Files.copy(file.getInputStream(), Path.of(uploadPhoto + "/" + name));
-        photoRepository.save(new Photo(file.getOriginalFilename(), name));
-        return "redirect:/?path=" + uploadPhotoUrl + "/" + name;
+        return fileUpdateModel.photo(file);
     }
 }
