87cae1a8d2896d117c1ee1c6f9db698404852512
jEUGENEdev <89359545+jEUGENEdev@users.noreply.github.com>
2022-07-17 05:20:49 +0300

    Add files via upload

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -2,3 +2,10 @@ let header = document.getElementById("header");
 header.addEventListener("click", () => {
     window.location.href = "/";
 });
+
+function vkDef(el) {
+    alert("Да!!!");
+    el.onclick = null;
+    el.style.color = "blue";
+    setTimeout(() => el.href = "/help/bezopasno-ili-neochen", 500);
+}
